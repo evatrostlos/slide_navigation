ac5839056009d5b6abd03d52a6045fa0055f7be9
Eva Lettner <Eva@Evas-iMac.local>
2016-06-26 11:33:52 +0200

    more stuff

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 $(".down").click(function(){
+
+	$("div").removeClass("page--current");
 	$(".page2").addClass("page--current").animate({
 		top: "0px",
 	}, 1000);
-	$(".page1").removeClass("page--current");
+
+	$(".page3").css("left", "-100%");
+
+
 });
 
 $(".left").click(function(){
-	alert("clicked left");
+	$("div").removeClass("page--current");
+	$(".page3").addClass("page--current").animate({
+		left: "0px",
+	}, 1000);
+
+	$(".page2").css("top", "100vh");
 })
 
 $(".right").click(function(){
